1bdfd6555afa17ca4de9c2377784ec5587272cd5
Fredrik Rinsén <fredrikh@outlook.com>
2021-02-01 23:18:13 +0100

    Started to add stuff into db context

---
+++ src/Rinsen.IdentityProvider/Outback/OutbackDbContext.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rinsen.IdentityProvider.Outback.Entities
+{
+    public class OutbackDbContext : DbContext
+    {
+        public OutbackDbContext(DbContextOptions<OutbackDbContext> optionsBuilder)
+            : base(optionsBuilder)
+        {
+        }
+
+        public DbSet<OutbackAllowedCorsOrigin> AllowedCorsOrigins { get; set; }
+        public DbSet<OutbackClient> Clients { get; set; }
+        public DbSet<OutbackClientClaim> ClientClaims { get; set; }
+        public DbSet<OutbackClientLoginRedirectUri> ClientLoginRedirectUris { get; set; }
+        public DbSet<OutbackClientPostLogoutRedirectUri> ClientPostLogoutRedirectUris { get; set; }
+        public DbSet<OutbackClientScope> ClientScopes { get; set; }
+        public DbSet<OutbackClientSecret> ClientSecrets { get; set; }
+        public DbSet<OutbackClientSupportedGrantType> ClientSupportedGrantTypes { get; set; }
+        public DbSet<OutbackCodeGrant> CodeGrants { get; set; }
+        public DbSet<OutbackPersistedGrant> PersistedGrants { get; set; }
+        public DbSet<OutbackRefreshTokenGrant> RefreshTokenGrants { get; set; }
+        public DbSet<OutbackScope> OutbackScopes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OutbackAllowedCorsOrigin>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClient>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientClaim>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientLoginRedirectUri>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientPostLogoutRedirectUri>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientScope>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientSecret>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackClientSupportedGrantType>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackCodeGrant>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackPersistedGrant>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackRefreshTokenGrant>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<OutbackScope>()
+                .HasKey(m => m.Id);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            SetCreatedUpdatedAndTimestampOnSave();
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            SetCreatedUpdatedAndTimestampOnSave();
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void SetCreatedUpdatedAndTimestampOnSave()
+        {
+            var saveStartTime = DateTimeOffset.Now;
+
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity is ICreatedAndUpdatedTimestamp && e.State == EntityState.Added))
+            {
+                ((ICreatedAndUpdatedTimestamp)entry.Entity).Created = saveStartTime;
+                ((ICreatedAndUpdatedTimestamp)entry.Entity).Updated = saveStartTime;
+            }
+
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity is ICreatedAndUpdatedTimestamp && e.State == EntityState.Modified))
+            {
+                ((ICreatedAndUpdatedTimestamp)entry.Entity).Updated = saveStartTime;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+
